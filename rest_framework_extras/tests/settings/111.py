26b28c2e7624fbbab3515f3a6c5ad53460ac62ad
Wim Keirsgieter <wim@.keirsgieter@gmail.com>
2021-05-10 15:43:47 +0200

    Wrong middleware class

--- rest_framework_extras/tests/settings/111.py
+++ rest_framework_extras/tests/settings/111.py
@@ -32,7 +32,7 @@ ROOT_URLCONF = "rest_framework_extras.tests.urls"
 MIDDLEWARE_CLASSES = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.auth.middleware.SessionAuthenticationMiddleware",
-    "django.contrib.auth.middleware.MessageMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
 )
