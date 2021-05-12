94865eae8f76158337c9492dcf72290461a18b7a
Wim Keirsgieter <wim@.keirsgieter@gmail.com>
2021-05-12 13:40:31 +0200

    Include boilerplate settings module

---
+++ project/settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'secret'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": "drfe.db",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    }
+}
